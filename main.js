a3eb4f44cb73ef24cd6b10cb80361e533aa4c083
pragatikohli <pragatikohli12@gmail.com>
2022-08-11 15:23:10 +0530

    Home Styling Done

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function stickyNavbar() {
 }
 stickyNavbar();
 window.addEventListener("scroll", stickyNavbar);
+//Show Case Area Animation
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: "60px",
+});
+sr.reveal(".showcase-info", { delay: 600 });
+sr.reveal(".showcase-image", { origin: "top", delay: 700 });
