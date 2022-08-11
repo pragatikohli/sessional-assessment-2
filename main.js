717d91bac0602e8e5de6657c23620190f17b35fb
pragatikohli <pragatikohli12@gmail.com>
2022-08-11 17:42:59 +0530

    Hamburger Menu Added

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ let sr = ScrollReveal({
 });
 sr.reveal(".showcase-info", { delay: 600 });
 sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+//Open and Close Navbar Menu
+const links = document.querySelectorAll(".nav-link");
+const togglebtn = document.querySelector(".toggle-btn");
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  document.body.classList.toggle("stopScrolling");
+});
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  })
+);
